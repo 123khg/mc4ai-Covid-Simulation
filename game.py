92a0406b120f286f28e0a473efa2dba14836e86a
123khg <khgallgaming@gmail.com>
2023-05-02 20:32:22 +0700

    Screen plotting notes for Paul

--- game.py
+++ game.py
@@ -1,12 +1,30 @@
 import streamlit as st, plotly as plt, numpy as np, base64, time
 
 ''' Notes for later development:
-Make a code that works out a (800, st.screen_width/3) dimension plot for the Piano game mode
 Create function to receive position of note, time appear and duration (length) of note to create it on the plot
-Warning: do not make the plot shorter (happens when an object's coords is higher than any other which makes the plot shorten)
-Hint: try drawing it right at the boundary and only gets bigger when it move downwards
 Style the plot to similar ( dont copy :))) ) to Osu Mania
 
 Later improvements would be 2 new game modes: Osu taiko
 Same requirements: function that receives time appear and duration of notes (no position this time)
 '''
+
+#The part for screen is already made over on app.py so it will be copied here later
+#Only need to make a function that takes in these and output a figure that app.py can use to plot
+#width and height are floats;
+#notes is a list that has 4 elements which indicates the position of the note on the plot
+#duration is a list that also has 4 elements which has the y-length of the note
+'''
+Heres a hint: use matplotlib plt.bar(x, height, width, y_coord, edge_color = "black"
+'''
+def Piano(width, height, notes, duration):
+    pass
+    return fig
+    
+    
+    
+def Drums(width, height, notes, duration):
+    pass
+    return fig
+    
+    
+def Catch(): pass
